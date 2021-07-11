cd4a9cd9d30b47dba151084b6cb844d3b00a0237
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-07-11 16:02:09 +0300

    Main page adaptation

--- dev/components/header/header.js
+++ dev/components/header/header.js
@@ -1,4 +1,17 @@
+let offset = 0;
 const $banner = document.querySelector('.header__banner');
+const hideOverflow = () => {
+  offset = -parseInt(document.body.style.marginTop);
+  document.body.style.cssText = `position: fixed; margin-top: ${-window.scrollY}px`;
+};
+const showOverflow = offset => {
+  document.body.style.cssText = 'position: static; margin-top: 0';
+  window.scrollTo(0, offset);
+};
+const hideHeaderMenu = () => {
+  showOverflow();
+  document.querySelector('.header-menu').classList.remove('header-menu--shown');
+};
 
 if (!JSON.parse(localStorage.getItem('bannerIsHidden'))) {
   $banner.style.display = 'flex';
@@ -12,3 +25,11 @@ if (!JSON.parse(localStorage.getItem('bannerIsHidden'))) {
 } else {
   $banner.remove();
 }
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+  hideOverflow();
+  document.querySelector('.header-menu').classList.add('header-menu--shown');
+});
+
+document.querySelector('.header-menu__backdrop').addEventListener('click', hideHeaderMenu);
+document.querySelector('.header-menu__close').addEventListener('click', hideHeaderMenu);
--- dev/components/reviews/reviews.js
+++ dev/components/reviews/reviews.js
@@ -1,10 +1,16 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   spaceBetween: 20,
-  slidesPerView: 3,
-  slidesPerGroup: 3,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
   loopAdditionalSlides: 3,
   pagination: {
     el: '.swiper-pagination'
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup: 3
+    }
   }
 });
